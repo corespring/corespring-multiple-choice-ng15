b20dd3acd525f8c5f1751c09ad89fdbe54cb830d
Ed Eustace <ed.eustace@gmail.com>
2017-02-07 13:06:16 +0000

    remove dependency on lodash.debounce and less

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import debounce from 'lodash.debounce';
 //TODO: We are importing a transitive dependency here - which is unsafe.
-import CorespringNg15Element from 'corespring-legacy-component-dependencies/support/corespring-ng15-element'
-import 'corespring-legacy-multiple-choice'
+import CorespringNg15Element from 'corespring-legacy-component-dependencies/support/corespring-ng15-element';
+import 'corespring-legacy-multiple-choice';
 
 export default class CorespringMultipleChoiceNg15Element extends CorespringNg15Element {
   _legacyHtml() {
